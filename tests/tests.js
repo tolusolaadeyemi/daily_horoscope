d97b61c5a3e791a3f82b94430de1b0c30e557d05
toluwanimi sola-adeyemi <tolusa@hotmail.com>
2022-09-09 16:16:10 +0100

    async changes

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,8 @@
 
 const Horoscope = require('../lib/Horoscope');
+// const result = Horoscope.getAsyncHoroscope({ day: 25, month: 6 }).then(function(result){
+//     console.log('here',result);
+// });
+console.log('promise',Horoscope.getAsyncHoroscope({ day: 25, month: 6 })); 
 
-console.log(Horoscope.getHoroscope({ day: 25, month: 6 })); 
 
